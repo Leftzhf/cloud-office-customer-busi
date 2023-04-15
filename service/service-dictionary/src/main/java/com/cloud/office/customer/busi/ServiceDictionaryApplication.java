77f364c865cd79d7cfaceda6c56829b76f961a9d
leftzhf <acemagic123@outlook.com>
2023-04-16 01:26:58 +0800

    feat: 完成所有服务的初步整合，正常启动

--- service/service-dictionary/src/main/java/com/cloud/office/customer/busi/ServiceDictionaryApplication.java
+++ service/service-dictionary/src/main/java/com/cloud/office/customer/busi/ServiceDictionaryApplication.java
@@ -3,6 +3,7 @@ package com.cloud.office.customer.busi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author ZuoHaoFan
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.cloud.office.customer.busi"})
 public class ServiceDictionaryApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceDictionaryApplication.class, args);
